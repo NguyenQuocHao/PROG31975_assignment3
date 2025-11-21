c20128c594bdbb04b977be4d020d4872d1a3d9a7
Hao Nguyen <nguyenquochao2000@gmail.com>
2025-11-21 15:03:50 -0500

    Initial Commit

---
+++ Assignment3Hao/Assignment3HaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3HaoApp.swift
+//  Assignment3Hao
+//
+//  Created by user275773 on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3HaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
